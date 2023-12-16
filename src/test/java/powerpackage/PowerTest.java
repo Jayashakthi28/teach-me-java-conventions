43c5903196a3148ea697ea5ef8a88b99d1f36f54
Jayashakthi Vishnu <vishnu28.js@gmail.com>
2023-12-16 18:42:41 +0530

    change the dependency

--- src/test/java/powerpackage/PowerTest.java
+++ src/test/java/powerpackage/PowerTest.java
@@ -3,24 +3,26 @@ package powerpackage;
 
 import org.junit.Test;
 
-public class powertest {
+public class PowerTest {
+
+    PowerFinder powerfinder=new PowerFinder();
     @Test
-    public void one_raised_to_one_is_onew() {
+    public void one_raised_to_one_is_one() {
         assert powerfinder.OF(1,1) == 1;
     }
 
     @Test
-    public void twoRaised_to_one_is_2() {
+    public void twoRaised_to_one_is_two() {
         assert powerfinder.OF(2,1) == 2;
     }
 
     @Test
-    public void two_power_2_is_4() {
+    public void two_power_two_is_four() {
         assert powerfinder.OF(2, 2) == 4;
     }
 
     @Test
-    public void power_of_2_and_3_Is_SIX() {
+    public void power_of_two_and_three_is_six() {
         assert powerfinder.OF(3, 2) == 3*3;
     }
 }
