d9b6c10fe4154aac0785973f7c6aeb549ae574d5
Jayashakthi Vishnu <vishnu28.js@gmail.com>
2023-12-16 18:43:21 +0530

    refactor the code to make the tests work

--- src/test/java/powerpackage/PowerTest.java
+++ src/test/java/powerpackage/PowerTest.java
@@ -4,25 +4,24 @@ package powerpackage;
 import org.junit.Test;
 
 public class PowerTest {
-
-    PowerFinder powerfinder=new PowerFinder();
+    
     @Test
     public void one_raised_to_one_is_one() {
-        assert powerfinder.OF(1,1) == 1;
+        assert PowerFinder.of(1,1) == 1;
     }
 
     @Test
     public void twoRaised_to_one_is_two() {
-        assert powerfinder.OF(2,1) == 2;
+        assert PowerFinder.of(2,1) == 2;
     }
 
     @Test
     public void two_power_two_is_four() {
-        assert powerfinder.OF(2, 2) == 4;
+        assert PowerFinder.of(2, 2) == 4;
     }
 
     @Test
     public void power_of_two_and_three_is_six() {
-        assert powerfinder.OF(3, 2) == 3*3;
+        assert PowerFinder.of(3, 2) == 3*3;
     }
 }
