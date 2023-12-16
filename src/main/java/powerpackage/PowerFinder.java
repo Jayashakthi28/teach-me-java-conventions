66618e36be2cbfcf115465dd0c702598afbc24d8
Jayashakthi Vishnu <vishnu28.js@gmail.com>
2023-12-16 18:43:01 +0530

    refactor the code with clean variable names

--- src/main/java/powerpackage/PowerFinder.java
+++ src/main/java/powerpackage/PowerFinder.java
@@ -1,11 +1,11 @@
 package powerpackage;
 
 public class PowerFinder {
-    public static int OF(int xyz, int pqr) {
-        int p = 1;
-        for (int i = 0; i<pqr; i++) {
-            p *= xyz;
+    public static int of(int base, int pow) {
+        int result = 1;
+        for (int i = 0; i<pow; i++) {
+            result *= base;
         }
-        return p;
+        return result;
     }
 }
